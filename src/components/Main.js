e26e882884e8d3fa87a99a522c21c1b9d824701b
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-01-18 15:42:19 -0500

    remove pym calls

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,23 +4,9 @@ import Body from "./Body";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar"
 
-import {pymSendHeight} from '../utils/handlePym'
 import SearchResults from "./SearchResults";
 
 class Main extends React.Component {
-
-  componentDidMount() {
-    // This is intended to fix bug where app is clipped at bottom
-    // on initial load.
-    pymSendHeight({timeout: 500})
-    pymSendHeight({timeout: 1000})
-  }
-
-  componentDidUpdate() {
-    // Because our app changes height based on displayed content, we 
-    // update the iframe height after DOM elements have been updated.
-    pymSendHeight()
-  }
 
   render() {
     return (
