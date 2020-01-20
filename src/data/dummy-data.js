24b7a3cce6fdb1b71988e6cf4e6985e557c8efd2
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-01-19 20:39:14 -0500

    improve dummy data with fake vals

--- src/data/dummy-data.js
+++ src/data/dummy-data.js
@@ -1,19 +1,38 @@
 import BAKER from "./pdfs/PA-BAKER-MATTHEW-E.pdf"
 import COSTA from "./pdfs/PA-COSTA-JR-JAY.pdf"
+import faker from "faker"
+import _ from "lodash"
 
-export const DATA = [
-  {
-  "01_first_name":"Matthew",
-  "01_last_name":"Baker",
-  "01_middle_initial":"E",
-  "01_suffix":"",
-  "meta_pdf_path": BAKER
-},
-{
-  "01_first_name":"Jay",
-  "01_last_name":"Costa",
-  "01_middle_initial":"",
-  "01_suffix":"Jr",
-  "meta_pdf_path": COSTA
-},
-]
+const fakeData = (num) => {
+  const data = []
+  for (var i = 0; i < num + 1; i++) {
+    data.push({
+  "q01_first_name": faker.name.firstName(),
+  "q01_last_name": faker.name.lastName(),
+  "q01_middle_initial": faker.name.firstName()[0],
+  "q01_suffix":faker.name.suffix,
+  "meta_filing_year": _.sample(["2015","2016","2017","2018"]),
+  "meta_pdf_path": BAKER    
+})
+  }
+  return data
+}
+
+export const DATA = fakeData(20)
+
+// export const DATA = [
+//   {
+//   "q01_first_name":"Matthew",
+//   "q01_last_name":"Baker",
+//   "q01_middle_initial":"E",
+//   "q01_suffix":"",
+//   "meta_pdf_path": BAKER
+// },
+// {
+//   "q01_first_name":"Jay",
+//   "q01_last_name":"Costa",
+//   "q01_middle_initial":"",
+//   "q01_suffix":"Jr",
+//   "meta_pdf_path": COSTA
+// },
+// ]
