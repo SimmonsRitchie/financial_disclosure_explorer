95e1a7baa3d117933e811db25d68fbdf2b25730b
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-01-20 09:32:05 -0500

    attempt to improve search algorithm - still not working well

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,16 +3,28 @@ import { SearchContext } from "../context/SearchContext";
 import SearchResultsItem from "./SearchResultsItem";
 
 const applySearchTerms = (searchText, searchData) => {
-  return searchData.filter(item => {
-    const fullname =
-      item.q01_first_name +
-      " " +
-      item.q01_middle_initial +
-      " " +
-      item.q01_last_name;
-    const regex = new RegExp(searchText, "gi");
-    return fullname.match(regex);
-  });
+  if (searchText === "") {
+    return searchData;
+  } else {
+    return searchData.filter(item => {
+      // get all item fields
+      const searchFields = Object.keys(item);
+      // split search terms up if white space present
+      const searchTextChunks = searchText.split(" ")
+      for (const field of searchFields) {
+        const chunkMatchCount = 0
+        for (const chunk of searchTextChunks) {
+          const regex = new RegExp(chunk, "gi");
+          if (item[field].match(regex)) {
+            chunkMatchCount += 1
+          }
+        }
+        if (chunkMatchCount === searchTextChunks.length) {
+          return item
+        }
+      }
+    });
+  }
 };
 
 const SearchResults = props => {
@@ -20,7 +32,9 @@ const SearchResults = props => {
   const filteredData = applySearchTerms(searchText, props.data);
   return (
     <section>
-    <h2 className="subtitle">Showing <strong>{filteredData.length}</strong> forms</h2>
+      <h2 className="subtitle">
+        Showing <strong>{filteredData.length}</strong> forms
+      </h2>
       <div className="container">
         {filteredData.map((item, idx) => {
           return <SearchResultsItem key={idx} item={item} />;
