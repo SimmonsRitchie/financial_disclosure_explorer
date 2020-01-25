beb49a2db25a8334a86842bc095bf6536d0c50aa
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-01-25 12:50:18 -0500

    improve styles

--- src/components/SearchResultsItem.js
+++ src/components/SearchResultsItem.js
@@ -18,9 +18,14 @@ const SearchResultsItem = props => {
   }
 
   return (
-      <div className="box" onClick={handleClick}>
-        <h2 className="title is-size-5">{q01_first_name} {q01_middle_initial}. {q01_last_name}</h2>
-        <h3 className="subtitle is-size-6">Filing year: {meta_filing_year}, Filing as: {q04_public_position}</h3>
+      <div className="box" >
+      <div className="results-item__container-header">
+        <div className="results-item__container-basic-details">
+          <h2 className="title is-size-5">{q01_first_name} {q01_middle_initial}. {q01_last_name}</h2>
+          <h3 className="subtitle is-size-6">Filing year: {meta_filing_year}, Filing as: {q04_public_position}</h3>
+        </div>
+        <button onClick={handleClick}>EXPAND</button>
+      </div>
         {expand && <SearchResultsItemInspect />}
       </div>
   );
--- src/components/SearchResultsItemInspect.js
+++ src/components/SearchResultsItemInspect.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const SearchResultsItemInspect = () => {
-  return ( 
-    <div>
-    <h1>Expanded!</h1>
-    </div> );
-}
- 
+  return (
+    <div className="results-item-inspect__container">
+      <Tabs>
+        <TabList>
+          <Tab>Extracted text</Tab>
+          <Tab>PDF</Tab>
+        </TabList>
+
+      <TabPanel >
+        <h2>Extracted text</h2>
+      </TabPanel>
+
+      <TabPanel >
+        <h2>PDF view!</h2>
+      </TabPanel>
+
+      </Tabs>
+    </div>
+  );
+};
+
 export default SearchResultsItemInspect;
