f10a7bcb0d430909cabeb3135d5294910d0efcb0
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-01-25 20:27:50 -0500

    alter clickable area

--- src/components/SearchResultsItem.js
+++ src/components/SearchResultsItem.js
@@ -28,12 +28,12 @@ const SearchResultsItem = (props) => {
 
   return (
       <div className="box" >
-      <div className="results-item__container-header">
+      <div className="results-item__container-header" onClick={handleClick}>
         <div className="results-item__container-basic-details">
           <h2 className="title is-size-5">{q01_first_name} {q01_middle_initial}. {q01_last_name}</h2>
           <h3 className="subtitle is-size-6">Filing year: {meta_filing_year}, Filing as: {q04_public_position}</h3>
         </div>
-        <div onClick={handleClick}>{expandText}</div>
+        <div >{expandText}</div>
       </div>
         {expand && <SearchResultsItemInspect item={props.item} />}
       </div>
