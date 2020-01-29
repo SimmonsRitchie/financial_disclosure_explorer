c6494761f64f0afe9e14db3337a7615b9372364a
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-01-28 20:08:18 -0500

    replace bulma tabs with customed-styled a tags - subtler look

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,37 +3,45 @@ import React, { useState, useContext } from "react";
 import SearchBarQuick from "./SearchBarQuick";
 import SearchBarAdvanced from "./SearchBarAdvanced";
 
-const SearchBar = (props) => {
-  const [searchType, setSearchType] = useState('quick')
-  const SearchBarType = searchType === 'quick' ? 
-    <SearchBarQuick/> : 
-    <SearchBarAdvanced/>
+const SearchBar = props => {
+  const [searchType, setSearchType] = useState("quick");
+  const SearchBarType =
+    searchType === "quick" ? <SearchBarQuick /> : <SearchBarAdvanced />;
 
-
-  const handleSearchType = (searchType) => {
-    if (searchType == 'quick') {
-        setSearchType('quick')
-    } else if (searchType == 'advanced') {
-      setSearchType('advanced')
+  const handleSearchType = searchType => {
+    if (searchType == "quick") {
+      setSearchType("quick");
+    } else if (searchType == "advanced") {
+      setSearchType("advanced");
     }
-  }
+  };
 
   return (
     <div className="section">
       <div className="container">
-        <div className="tabs is-centered is-toggle is-small" >
-          <ul>
-            <li className={searchType === 'quick' ? "is-active" : ""} onClick={() => handleSearchType('quick')}>
-              <a>Quick search</a>
-            </li>
-            <li className={searchType === 'advanced' ? "is-active" : ""} onClick={() => handleSearchType('advanced')}>
-              <a>Advanced search</a>
-            </li>
-          </ul>
+        <div className="searchbar__option-container">
+          <a
+            className={
+              searchType == "quick"
+                ? "searchbar__option-active"
+                : "searchbar__option"
+            }
+            onClick={() => handleSearchType("quick")}
+          >
+            Quick Search
+          </a>
+          <a
+            className={
+              searchType == "advanced"
+                ? "searchbar__option-active"
+                : "searchbar__option"
+            }
+            onClick={() => handleSearchType("advanced")}
+          >
+            Advanced Search
+          </a>
         </div>
-        <div>
-          {SearchBarType}
-        </div>
+        <div>{SearchBarType}</div>
       </div>
     </div>
   );
