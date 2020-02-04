6e35590c26116ba2e036f1dadcc8d38c14b238fd
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-02-04 16:46:51 -0500

    create fields var

--- src/config/searchSettings.js
+++ src/config/searchSettings.js
@@ -2,79 +2,106 @@
 
 //TODO: Create fields array specifying what fields to search or display
 
-const FIELDS = []
-
-export const DISPLAY_FIELDS = []
-
-export const SEARCH_FIELDS = [
+export const FIELDS = [
   {
-    display: "Filing year",
+    display_name: "Filing year",
     value: "meta_filing_year",
-    inputType: "select"
+    inputType: "select",
+    search: true,
+    display: false
   },
   {
-    display: "First name",
+    display_name: "First name",
     value: "q01_first_name",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: false
   },
   {
-    display: "Last name",
+    display_name: "Last name",
     value: "q01_last_name",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: false
   },
   {
-    display: "Middle name",
+    display_name: "Middle name",
     value: "q01_middle_name",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: false
   },
   {
-    display: "Status",
+    display_name: "Status",
     value: "q03_status",
-    inputType: "select"
+    inputType: "select",
+    search: true,
+    display: true
   },
   {
-    display: "Public position",
+    display_name: "Public position",
     value: "q04_public_position",
-    inputType: "select"
+    inputType: "select",
+    search: true,
+    display: true
   },
   {
-    display: "Real estate interests",
+    display_name: "Real estate interests",
     value: "q08_real_estate_interests",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
   {
-    display: "Creditors",
+    display_name: "Creditors",
     value: "q09_creditors",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
   {
-    display: "Income",
+    display_name: "Income",
     value: "q10_income",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
   {
-    display: "Gifts",
+    display_name: "Gifts",
     value: "q11_gifts",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
   {
-    display: "Transportation, lodging, hospitality",
+    display_name: "Transportation, lodging, hospitality",
     value: "q12_transportation_lodging_hospitality",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
   {
-    display: "Office, directorship or employment",
+    display_name: "Office, directorship or employment",
     value: "q13_office_directorship_employment",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
   {
-    display: "Financial interests",
+    display_name: "Financial interests",
     value: "q14_financial_interest",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
   {
-    display: "Business interests",
+    display_name: "Business interests",
     value: "q15_business_interests",
-    inputType: "text"
+    inputType: "text",
+    search: true,
+    display: true
   },
 ]
+export const DISPLAY_FIELDS = FIELDS.filter(field => field.display)
+
+export const SEARCH_FIELDS = FIELDS.filter(field => field.search)
