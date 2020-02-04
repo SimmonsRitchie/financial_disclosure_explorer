f044784e7801ac9e839a0b17ae11e72dd258f762
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-02-04 16:47:24 -0500

    add table with item data

--- src/components/ExtractedText.js
+++ src/components/ExtractedText.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
+import { DISPLAY_FIELDS } from "../config/searchSettings";
 
-const ExtractedText = ({item}) => {
+const ExtractedText = ({ item }) => {
+  console.log("ITEM", item);
+  console.log(DISPLAY_FIELDS);
 
-  
-  return ( 
-    <div>
+  return (
+    <table className="table is-fullwidth">
+      <tbody>
+        {DISPLAY_FIELDS.map(field => (
+          <tr key={field.value}>
+            <td><strong>{field.display_name}</strong></td>
+            <td>{item[field.value]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-
-    </div> 
-    );
-}
- 
 export default ExtractedText;
