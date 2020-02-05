d11aa36563143cdb82bfece563c8397beecf0607
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-02-04 19:27:44 -0500

    add snippet view

--- src/components/SearchResultsItem.js
+++ src/components/SearchResultsItem.js
@@ -2,7 +2,9 @@ import React, {useState} from "react";
 import SearchResultsItemInspect from "./SearchResultsItemInspect";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import {htmlEscape} from "../utils/escape"
 
+//TODO: Add react-string-replace to highlight matching text
 
 const SearchResultsItem = (props) => {
   const { 
@@ -11,20 +13,27 @@ const SearchResultsItem = (props) => {
     q01_last_name,
     q04_public_position,
     meta_filing_year,
+    snippet
   } = props.item;
 
   const upArrow = <FontAwesomeIcon icon={faChevronUp} size="lg"/>
   const downArrow = <FontAwesomeIcon icon={faChevronDown} size="lg"/>
   const [expand, setExpand] = useState(false)
   const [expandText, setExpandText] = useState(downArrow)
+  const highlight = htmlEscape(snippet).replace(
+    /b4de2a49c8/g, '<b>'
+  ).replace(
+    /8c94a2ed4b/g, '</b>'
+  );
 
   const handleClick = () => {
     setExpand(!expand)
 
     const newExpandText = expand ? downArrow : upArrow
     setExpandText(newExpandText)
-
   }
+
+
 
   return (
       <div className="box" >
@@ -32,6 +41,7 @@ const SearchResultsItem = (props) => {
         <div className="results-item__container-basic-details">
           <h2 className="title is-size-5">{q01_first_name} {q01_middle_name && `${q01_middle_name[0]}.`} {q01_last_name}</h2>
           <h3 className="subtitle is-size-6">Filing year: {meta_filing_year}, Filing as: {q04_public_position}</h3>
+          {highlight && <span className="is-italic">...{highlight}...</span>}
         </div>
         <div >{expandText}</div>
       </div>
--- src/utils/escape.js
+++ src/utils/escape.js
@@ -1,13 +1,12 @@
-export const htmlEscape = (s) => {
+export const htmlEscape = s => {
+  if (!s) {
+    return "";
+  }
   /* Escapes HTML in string*/
-  return s.replace(
-  />/g, '&gt;'
-).replace(
-  /</g, '&lt;'
-).replace(
-  /&/g, '&'
-).replace(
-  /"/g, '&quot;'
-).replace(
-  /'/g, '&#039;'
-);}
+  return s
+    .replace(/>/g, "&gt;")
+    .replace(/</g, "&lt;")
+    .replace(/&/g, "&")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+};
