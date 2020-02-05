c6cc70c6010c5db83f61b9b56be94f39703a1655
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-02-04 20:58:35 -0500

    improve matching word highlighting

--- src/components/SearchResultsItem.js
+++ src/components/SearchResultsItem.js
@@ -3,6 +3,7 @@ import SearchResultsItemInspect from "./SearchResultsItemInspect";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import {htmlEscape} from "../utils/escape"
+import reactStringReplace from "react-string-replace"
 
 //TODO: Add react-string-replace to highlight matching text
 
@@ -20,11 +21,12 @@ const SearchResultsItem = (props) => {
   const downArrow = <FontAwesomeIcon icon={faChevronDown} size="lg"/>
   const [expand, setExpand] = useState(false)
   const [expandText, setExpandText] = useState(downArrow)
-  const highlight = htmlEscape(snippet).replace(
-    /b4de2a49c8/g, '<b>'
-  ).replace(
-    /8c94a2ed4b/g, '</b>'
-  );
+  const snippetEsc = htmlEscape(snippet)
+  // We highlight the matching words because we know they'll begin with 'b4de2a49c8' and end with '8c94a2ed4b'
+  // we also replace these terms in the text itself because sometimes a snippet contains more than one match but
+  // one part is cut off.
+  const highlight = reactStringReplace(snippetEsc, /b4de2a49c8(.*)8c94a2ed4b/gi, (match, i) => (
+    <span key={i} style={{ color: 'red' }}>{match.replace(/b4de2a49c8|8c94a2ed4b/gi,"")}</span>));
 
   const handleClick = () => {
     setExpand(!expand)
@@ -41,7 +43,7 @@ const SearchResultsItem = (props) => {
         <div className="results-item__container-basic-details">
           <h2 className="title is-size-5">{q01_first_name} {q01_middle_name && `${q01_middle_name[0]}.`} {q01_last_name}</h2>
           <h3 className="subtitle is-size-6">Filing year: {meta_filing_year}, Filing as: {q04_public_position}</h3>
-          {highlight && <span className="is-italic">...{highlight}...</span>}
+          {highlight && <span className="subtitle is-size-6 is-italic">...{highlight}...</span>}
         </div>
         <div >{expandText}</div>
       </div>
