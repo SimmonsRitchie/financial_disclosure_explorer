9d229251848bcad3a70658219b37301b54c840ba
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-02-05 11:27:51 -0500

    change from table to divs so info displays better on mobile

--- src/components/ExtractedText.js
+++ src/components/ExtractedText.js
@@ -6,16 +6,14 @@ const ExtractedText = ({ item }) => {
   console.log(DISPLAY_FIELDS);
 
   return (
-    <table className="table is-fullwidth">
-      <tbody>
+    <div>
         {DISPLAY_FIELDS.map(field => (
-          <tr key={field.value}>
-            <td><span className="has-text-black-bis">{field.display_name}</span></td>
-            <td><span className="has-text-grey">{item[field.value]}</span></td>
-          </tr>
+          <div key={field.value}>
+            <div><span className="has-text-black-bis">{field.display_name}</span></div>
+            <div><span className="has-text-grey">{item[field.value]}</span></div>
+          </div>
         ))}
-      </tbody>
-    </table>
+    </div>
   );
 };
 
