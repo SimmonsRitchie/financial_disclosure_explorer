bcd2f11e8166d4a5bc3ee960468bfcd44f4e87cf
Daniel SR <daniel.simmons.ritchie@gmail.com>
2020-02-05 21:47:49 -0500

    tidy up

--- src/components/SearchResultsItem.js
+++ src/components/SearchResultsItem.js
@@ -1,14 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SearchResultsItemInspect from "./SearchResultsItemInspect";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
-import {htmlEscape} from "../utils/escape"
-import reactStringReplace from "react-string-replace"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { htmlEscape } from "../utils/escape";
+import reactStringReplace from "react-string-replace";
 
-//TODO: Add react-string-replace to highlight matching text
-
-const SearchResultsItem = (props) => {
-  const { 
+const SearchResultsItem = props => {
+  const {
     q01_first_name,
     q01_middle_name,
     q01_last_name,
@@ -18,39 +16,54 @@ const SearchResultsItem = (props) => {
     snippet
   } = props.item;
 
-  const upArrow = <FontAwesomeIcon icon={faChevronUp} size="lg"/>
-  const downArrow = <FontAwesomeIcon icon={faChevronDown} size="lg"/>
-  const [expand, setExpand] = useState(false)
-  const [expandText, setExpandText] = useState(downArrow)
-  const snippetEsc = htmlEscape(snippet)
+  const upArrow = <FontAwesomeIcon icon={faChevronUp} size="lg" />;
+  const downArrow = <FontAwesomeIcon icon={faChevronDown} size="lg" />;
+  const [expand, setExpand] = useState(false);
+  const [expandText, setExpandText] = useState(downArrow);
+  const snippetEsc = htmlEscape(snippet);
   // We highlight the matching words because we know they'll begin with 'b4de2a49c8' and end with '8c94a2ed4b'
   // we also replace these terms in the text itself because sometimes a snippet contains more than one match but
   // one part is cut off.
-  const snippetReplace = reactStringReplace(snippetEsc, /b4de2a49c8(.*)8c94a2ed4b/gi, (match, i) => (
-    <span key={i} style={{ color: 'red' }}>{match.replace(/b4de2a49c8|8c94a2ed4b/gi,"")}</span>));
+  const snippetReplace = reactStringReplace(
+    snippetEsc,
+    /b4de2a49c8(.*)8c94a2ed4b/gi,
+    (match, i) => (
+      <span key={i} style={{ color: "red" }}>
+        {match.replace(/b4de2a49c8|8c94a2ed4b/gi, "")}
+      </span>
+    )
+  );
   const highlight = snippetEsc ? snippetReplace : null;
 
   const handleClick = () => {
-    setExpand(!expand)
+    setExpand(!expand);
 
-    const newExpandText = expand ? downArrow : upArrow
-    setExpandText(newExpandText)
-  }
-
-
+    const newExpandText = expand ? downArrow : upArrow;
+    setExpandText(newExpandText);
+  };
 
   return (
-      <div className="box" >
+    <div className="box">
       <div className="results-item__container-header" onClick={handleClick}>
         <div>
-          <div className="has-text-weight-bold is-size-5">{q01_first_name} {q01_middle_name && `${q01_middle_name[0]}.`} {q01_last_name}</div>
-          <div className="is-size-6">Filing year: {q07_filing_year}, Filing as: {q03_status}, {q04_state_or_local}</div>
-          {highlight && <div className="results-item__preview" ><span className="is-size-6 is-italic">...{highlight}...</span></div>}
+          <div className="has-text-weight-bold is-size-5">
+            {q01_first_name} {q01_middle_name && `${q01_middle_name[0]}.`}{" "}
+            {q01_last_name}
+          </div>
+          <div className="is-size-6">
+            Filing year: {q07_filing_year}, Filing as: {q03_status},{" "}
+            {q04_state_or_local}
+          </div>
+          {highlight && (
+            <div className="results-item__preview">
+              <span className="is-size-6 is-italic">...{highlight}...</span>
+            </div>
+          )}
         </div>
-        <div >{expandText}</div>
+        <div>{expandText}</div>
       </div>
-        {expand && <SearchResultsItemInspect item={props.item} />}
-      </div>
+      {expand && <SearchResultsItemInspect item={props.item} />}
+    </div>
   );
 };
 
